f968a84cfd328992c2030e4f455bf6d794a8901f
Leonardo <lopezleonardo282@gmail.com>
2017-10-04 19:23:31 -0600

    Se crea la estructura controladora de Sesiones

--- Rent-a-Car/Rent-a-Car/mdlDeclaracionClases.vb
+++ Rent-a-Car/Rent-a-Car/mdlDeclaracionClases.vb
@@ -3,4 +3,64 @@
     Public Agencia As clsAgencias 'Variable para el tipo de clase clsAgencia
     Public Clientes As clsClientes 'Variable para el tipo de clase clsClientes
     Public Coches As clsCoches 'Variable para el tipo de clase clsCoches
+
+    'Estructura creada con el fin de manejo de sesiones
+    Public Session As EstructuraSession = New EstructuraSession(Nothing, Nothing)
+    Structure EstructuraSession
+        Private _tipoUsuario As String
+        Private _idUsuario As String
+
+        Public ReadOnly Property ObtenerTipoUsuario() As String
+            Get
+                Return _tipoUsuario
+            End Get
+        End Property
+
+        Public WriteOnly Property EstablecerCodigoUsuario() As String
+            Set(ByVal value As String)
+                _tipoUsuario = value
+            End Set
+        End Property
+
+        Public ReadOnly Property ObtenerIdUsuario() As String
+            Get
+                Return _idUsuario
+            End Get
+        End Property
+
+        Public WriteOnly Property EstablecerIdUsuario() As String
+            Set(ByVal value As String)
+                _idUsuario = value
+            End Set
+        End Property
+
+        Public Sub New(ByVal _tipo As String, ByVal _id As String)
+            _tipoUsuario = _tipo
+            _idUsuario = _id
+        End Sub
+
+        Public Sub CerrarSession()
+            _tipoUsuario = Nothing
+            _idUsuario = Nothing
+            Dim formTitles As New Collection
+            frmInicio_Sesion.Show()
+
+            For Each f As Form In My.Application.OpenForms 'Se cierran los forms abiertos
+                f.Hide()
+            Next
+        End Sub
+
+        Public Sub Enrutador(ByRef formActual As Form)
+            If _tipoUsuario <> Nothing And _idUsuario <> Nothing Then
+                If _tipoUsuario = "A" Then 'Administrador
+                    frmMenu_Admin.Show()
+                ElseIf _tipoUsuario = "G" Then 'Gerente
+                    frmMenu_Agentes.Show()
+                ElseIf _tipoUsuario = "C" Then 'Contador
+                    frmMenu_contador.Show()
+                End If
+                formActual.Hide()
+            End If
+        End Sub
+    End Structure
 End Module
